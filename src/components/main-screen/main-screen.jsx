65934f4797f04daa624186ca612714f5c0f0ca8d
nikitenkovit <nelot@mail.ru>
2021-02-06 01:50:13 +0300

    finished task 2-1

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PlaceCard from "../place-card";
 import PropTypes from "prop-types";
 
-const MainPage = ({placeCardsIds, quantityRentalOffers}) => {
+const MainScreen = ({placeCardsIds, quantityRentalOffers}) => {
   return (
     <div className="page page--gray page--main">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
+              <Link className="header__logo-link header__logo-link--active" to="/">
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
+              </Link>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <Link className="header__nav-link header__nav-link--profile" to="/favorites">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </nav>
@@ -104,9 +105,9 @@ const MainPage = ({placeCardsIds, quantityRentalOffers}) => {
   );
 };
 
-MainPage.propTypes = {
+MainScreen.propTypes = {
   placeCardsIds: PropTypes.arrayOf(PropTypes.string.isRequired),
   quantityRentalOffers: PropTypes.number.isRequired
 };
 
-export default MainPage;
+export default MainScreen;
