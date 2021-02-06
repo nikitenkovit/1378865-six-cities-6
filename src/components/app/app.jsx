6d5877b7834b3c7c5d8d0fc65f4115b81aabbe7e
nikitenkovit <nelot@mail.ru>
2021-02-06 14:51:57 +0300

    corrected mistake

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainScreen from "../main-screen/main-screen";
-import SignInScreen from "../sign-in-screen/sign-In-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
 import FavoritesScreen from "../favorites-screen/favorites-screen";
 import RoomScreen from "../room-screen/room-screen";
 import NotFoundScreen from "../not-found-screen/not-found-screen";
